97eb7167218d457719c583ae3124ed8dd28d8ebe
Gustavo <gus.inacio@hotmail.com>
2021-11-11 08:45:18 -0300

    Dependencies installation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,9 +47,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-
-          <link rel="ico" href="https://rocketseat.com.br/favicon.ico" />
-
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
