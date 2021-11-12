d89a32f79607995a5d4f7e1eef8a97e4ac91d565
Gustavo <gus.inacio@hotmail.com>
2021-11-12 11:29:45 -0300

    impl: Last projects section.

---
+++ src/components/Projetos/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import SectionTitle from '../SectionTitle';
+import ProjectItem from './ProjectItem';
+import { Container } from './styles';
+
+function Projetos() {
+  return (
+    <Container>
+      <SectionTitle title="Ultimos Projetos" />
+      <section>
+        <ProjectItem
+          title="Trybetunes"
+          img="https://blog.nubank.com.br/wp-content/uploads/2019/09/Webp.net-resizeimage-2019-09-12T102711.144.jpg"
+          slug="test"
+        />
+        <ProjectItem
+          title="Trivia"
+          img="https://blog.nubank.com.br/wp-content/uploads/2019/09/Webp.net-resizeimage-2019-09-12T102711.144.jpg"
+          slug="test"
+        />
+        <ProjectItem
+          title="FrontEnd Online store"
+          img="https://blog.nubank.com.br/wp-content/uploads/2019/09/Webp.net-resizeimage-2019-09-12T102711.144.jpg"
+          slug="test"
+        />
+      </section>
+      <button type="button">
+        <Link href="/projects">
+          <a>Ver todos os projetos</a>
+        </Link>
+      </button>
+    </Container>
+  );
+}
+
+export default Projetos;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import HomeContainer from '../styles/HomeStyles';
 import HomeHero from '../components/HomeHero';
+import Projects from '../components/Projetos/index';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Header />
         <main className="container">
           <HomeHero />
+          <Projects />
         </main>
       </HomeContainer>
     </div>
