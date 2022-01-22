2b2955d1bbf2666df093773b9b1383ffb1e0de92
Gustavo <gus.inacio@hotmail.com>
2022-01-22 17:15:29 -0300

    trying to fix some bugs with cookies

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,16 +1,15 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { parseCookies, setCookie } from 'nookies';
 import dark from '../styles/theme/dark';
 import light from '../styles/theme/light';
 
 const cookies = parseCookies();
-
 const userTheme = cookies.USER_THEME;
 
 const DEFAULT_VALUE = {
   theme: {
-    title: userTheme === 'light' ? light.title : dark.title,
+    title: userTheme,
     colors: userTheme === 'light' ? light.colors : dark.colors
   },
   toggleTheme: () => {}
@@ -19,6 +18,12 @@ const DEFAULT_VALUE = {
 export const Context = createContext(DEFAULT_VALUE);
 
 export const ContextProvider: React.FC = ({ children }) => {
+  useEffect(() => {
+    setCookie(null, 'USER_THEME', 'light', {
+      maxAge: 86400 * 7,
+      path: '/'
+    });
+  }, []);
   const [theme, setTheme] = useState(DEFAULT_VALUE.theme);
   const toggleTheme = () => {
     if (theme.title === 'light') {
