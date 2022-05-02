6ca31d727d7234e85bd9de49780a67be3bae7cc5
Gustavo <gus.inacio@hotmail.com>
2022-05-02 07:54:18 -0300

    feat: Change the about me section.

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -29,33 +29,32 @@ function HomeHero() {
             <InfoItem data-aos="zoom-in">
               <section className="about">Sobre mim</section>
               <p>
-                Desenvolvedor <strong>Front End jr.</strong> altamente
-                comunicativo, dedicado e cheio de vontade de aprender cada vez
-                mais. Comecei minha jornada no{' '}
-                <strong>desenvolvimento web</strong> em junho de 2021 e desde
-                então, venho me esforçado todos os dias para evoluir minhas
-                habilidades tecnicas e sociocomportamentais a fim de poder
-                ingressar no mercado de trabalho e agregar valor.
+                Desenvolvedor <strong>Web</strong> altamente comunicativo,
+                dedicado e cheio de vontade de aprender cada vez mais. Comecei
+                minha jornada no <strong>desenvolvimento web</strong> em junho
+                de 2021 e desde então, venho me esforçado todos os dias para
+                evoluir minhas habilidades tecnicas e sociocomportamentais a fim
+                de poder ingressar no mercado de trabalho e agregar valor.
               </p>
             </InfoItem>
             <InfoItem data-aos="zoom-in">
               <p>
-                Na minha trajetoria como estudante, tenho trabalhado com
-                metodologias ágeis de desenvolvimento, de maneira prática, em
-                projetos em grupo e através de aulas voltadas ás soft skills no
-                curso da Trybe. Essas experiências serviram para me mostrar a
+                Na minha trajetoria como estudante e profissional, tenho
+                trabalhado com metodologias ágeis de desenvolvimento, como scrum
+                e kanban. Essas experiências serviram para me mostrar a
                 importancia de se ter um padrão organizacional de
                 desenvolvimento.
               </p>
             </InfoItem>
             <InfoItem data-aos="zoom-in">
               <p>
-                Atualmente, estou me aprofundando no <strong>Back End</strong>,
-                utilizando Node.js e express para criar APIs rest que se
-                comunicam diretamente com bancos de dados, até o momento tive
-                contato apenas com MySQL. Estou em busca de me tornar um
-                desenvolvedor full stack, pois gosto muito das duas vertentes e
-                pretendo, no futuro, trabalhar com as duas.
+                Atualmente, estou atuando como Front end e, em paralelo, me
+                aprofundando no <strong>Back End</strong>, utilizando Node.js e
+                express. No Back end, tenho familiaridade com sequelize, POO e
+                também já apliquei o método TDD em alguns projetos que
+                desenvolvi, em todos eles procurei utilizar autenticação via
+                JWT. Também ja tive contato com ferramentas de deploy, como
+                heroku e AWS.
               </p>
             </InfoItem>
           </InfosContainer>
