31fd1faf39c4323d49a9f86eb5b404e3fe9ca93a
Gustavo <gus.inacio@hotmail.com>
2022-06-14 08:52:41 -0300

    fix: Footer's style and responsivity

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,11 +1,11 @@
+import { lighten } from 'polished';
 import styled from 'styled-components';
 
 const Container = styled.footer`
   width: 100%;
   display: flex;
   margin-top: 10rem;
-  border-top: 1px solid ${({ theme }) => theme.colors.backgroundLight};
-  box-shadow: 0 0 4px ${({ theme }) => theme.colors.text};
+  box-shadow: 0 0 4px ${({ theme }) => lighten(0.3, theme.colors.text)};
   height: 5rem;
   align-items: center;
   justify-content: center;
@@ -21,13 +21,15 @@ const Container = styled.footer`
       color: ${({ theme }) => theme.colors.primary};
       text-transform: uppercase;
       font-weight: bold;
-      font-size: 1.2rem;
+      font-size: 1rem;
     }
 
     p {
       font-weight: bold;
       color: ${({ theme }) => theme.colors.text};
       justify-content: space-evenly;
+      text-align: center;
+      font-size: 1rem;
     }
 
     > section {
@@ -51,8 +53,13 @@ const Container = styled.footer`
 
     @media (max-width: 450px) {
       button {
-        font-size: 0.9rem;
+        font-size: 0.7rem;
       }
+
+      p {
+        font-size: 0.5rem;
+      }
+
       section {
         gap: 0.5rem;
         svg {
