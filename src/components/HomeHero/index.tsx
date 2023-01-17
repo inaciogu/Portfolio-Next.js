9577c61a30900b23db97f0265729f18d964944f0
gustavo <gus.inacio@hotmail.com>
2023-01-17 11:29:08 -0300

    fix: adjusts about me section

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -29,12 +29,7 @@ function HomeHero() {
             <InfoItem data-aos="zoom-in">
               <section className="about">Sobre mim</section>
               <p>
-                Desenvolvedor <strong>Web</strong> altamente comunicativo,
-                dedicado e cheio de vontade de aprender cada vez mais. Comecei
-                minha jornada no <strong>desenvolvimento web</strong> em junho
-                de 2021 e desde então, venho me esforçado todos os dias para
-                evoluir minhas habilidades tecnicas e sociocomportamentais a fim
-                de poder ingressar no mercado de trabalho e agregar valor.
+              Desenvolvedor de software cheio de vontade de aprender cada vez mais!  Gosto de viver novas experiências, aproveitar oportunidades de trabalhar com coisas novas e participar de novos desafios que me façam sair da minha zona de conforto. Encontrei na area de tecnologia uma paixão por desenvolver coisas que possam impactar de alguma forma a vida das pessoas, e por conta disso, me dedico todos os dias a fim de me tornar um profissional melhor.
               </p>
             </InfoItem>
             <InfoItem data-aos="zoom-in">
@@ -48,13 +43,7 @@ function HomeHero() {
             </InfoItem>
             <InfoItem data-aos="zoom-in">
               <p>
-                Atualmente, estou atuando como Front end e, em paralelo, me
-                aprofundando no <strong>Back End</strong>, utilizando Node.js e
-                express. No Back end, tenho familiaridade com sequelize, POO e
-                também já apliquei o método TDD em alguns projetos que
-                desenvolvi, em todos eles procurei utilizar autenticação via
-                JWT. Também ja tive contato com ferramentas de deploy, como
-                heroku e AWS.
+                Atualmente, atuo como desenvolvedor Full Stack em uma empresa focada em desenvolvimento de aplicativos para Zendesk, utilizando, principalmente, React.js e Node.js. Possuo experiência com ferramentas de cloud como AWS e heroku, com bancos de dados relacionais e não relacionais e testes com Jest. No momento, estou me aprofundando em padrões de projeto, principios da arquitetura limpa e de código limpo para dar o próximo passo na carreira.
               </p>
             </InfoItem>
           </InfosContainer>
